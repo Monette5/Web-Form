4e40cac51af8b8d93ba09fe2f15d111bdc7d11ec
monette5 <darcy4@hotmail.com>
2020-04-08 13:45:46 +0100

    Removed all configs with exmaple details

--- Alpha/app/resources/js/config.js
+++ Alpha/app/resources/js/config.js
@@ -9,13 +9,13 @@ window.appVars = {
 };
 // appSettings global variable - to set up all proxy calls.
 // - added to window to counter issues experienced in IE.
+// appSettings global variable - to set up all proxy calls.
+// - added to window to counter issues experienced in IE.
 window.appSettings = {
-  API_IDP_URL     : 'https://sbx-idp.mydexid.org',
-  APP_SESSION_URL : 'session.php',
-  API_KEY         : 'dkhg834kfbhs9dehngs04hng9sfgh34k', // For demo purposes only. 
-  CONN_ID         : '4042', // For demo purposes only.
-  PERMISSIONS_URL : appVars.local_env+'/permissions.php',
-  LOGIN_URL       : 'login.php',
-  PROXY_PDS_CREATE: 'proxy-pds-create.php',
-  RETURN_URL      : appVars.local_env+'/verification.php'
+  uid: $("uid"),
+  key: $('key'),
+  api_key: $("api_Key"),
+  source_type: "connection",
+  con_id: $('con_id'),
+  dataset: $('dataset')
 };
